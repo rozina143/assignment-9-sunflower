8bba871f9d1df8b0ed334b8ccd15ed20ccf001c6
rozina143 <rozinaakter083@gmail.com>
2025-10-29 02:45:45 +0600

    image setup

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -19,38 +19,38 @@ const Home = () => {
 
 
 <div className="p-4 space-y-12">
-     <div class="carousel w-full">
-  <div id="slide1" class="carousel-item relative w-full">
+     <div class="carousel w-[700px]">
+  <div id="slide1" class="carousel-item relative  w-[700px]">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-      class="w-full" />
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-NFmiJPjIoJ6_4y7I-gwZo5dTD41GRMYFKQ&s"
+      class="w-full h-[360px] object-cover " />
     <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
       <a href="#slide4" class="btn btn-circle">❮</a>
       <a href="#slide2" class="btn btn-circle">❯</a>
     </div>
   </div>
-  <div id="slide2" class="carousel-item relative w-full">
+  <div id="slide2" class="carousel-item relative  w-[700px]">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-      class="w-full" />
+      src="https://i.pinimg.com/736x/01/dd/0e/01dd0e60f2051dd19853303cf3eb1501.jpg"
+      class="w-full h-[360px] object-cover" />
     <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
       <a href="#slide1" class="btn btn-circle">❮</a>
       <a href="#slide3" class="btn btn-circle">❯</a>
     </div>
   </div>
-  <div id="slide3" class="carousel-item relative w-full">
+  <div id="slide3" class="carousel-item relative  w-[700px]">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-      class="w-full" />
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGnEx2L1QwBqUKHEVNTgHlCnCF114D-TreJw&s"
+      class="w-full h-[360px] object-cover" />
     <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
       <a href="#slide2" class="btn btn-circle">❮</a>
       <a href="#slide4" class="btn btn-circle">❯</a>
     </div>
   </div>
-  <div id="slide4" class="carousel-item relative w-full">
+  <div id="slide4" class="carousel-item relative  w-[700px]">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-      class="w-full" />
+      src="https://i.harperapps.com/covers/9780470111352/y648.jpg"
+      class="w-full h-[360px] object-cover" />
     <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
       <a href="#slide3" class="btn btn-circle">❮</a>
       <a href="#slide1" class="btn btn-circle">❯</a>
